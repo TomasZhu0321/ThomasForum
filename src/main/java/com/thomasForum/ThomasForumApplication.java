bff7b4d6879845f8a5beb4bced402aa6cdff2ae2
Thomas <tomaszhu0321@gmail.com>
2024-03-23 18:43:00 -0400

    thomasForum Project Initialization

---
+++ src/main/java/com/thomasForum/ThomasForumApplication.java
@@ -0,0 +1,13 @@
+package com.thomasForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThomasForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThomasForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomasForum/ThomasForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThomasForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
